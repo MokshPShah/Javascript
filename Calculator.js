73252edfa7db475728d9fa8f81f5a994a739e5ed
MokshPShah <shahmoksh90651@gmail.com>
2025-08-01 14:22:40 +0530

    Project Done

---
+++ Calculator.js
@@ -0,0 +1,14 @@
+let number1 = 25
+let number2 = 35
+
+const addition = number1 + number2
+const substraction = number1 - number2
+const Division = number1 / number2
+const Multiplication = number1 * number2
+const Modulus = number1 % number2
+
+console.log(`Addition of two numbers is: ${addition}`)
+console.log(`Substraction of two numbers is: ${substraction}`)
+console.log(`Division of two numbers is: ${Division}`)
+console.log(`Multiplication of two numbers is: ${Multiplication}`)
+console.log(`Modulus of two numbers is: ${Modulus}`)
